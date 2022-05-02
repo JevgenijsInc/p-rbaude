7ec0c01b5032350c230a5144244ae4a42c708b0e
JevgenijsInc <95209264+JevgenijsInc@users.noreply.github.com>
2022-05-02 05:52:47 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+brālis = int(input("Ievadiet sava brāļa dzīves gadu skaitu: "))
+māsa = int(input("Ievadiet māsas dzīves gadu skaitu: "))
+if brālis == māsa:
+  print("viņi ir dvīņi")
+else:
+  print("tie ir amogus")
