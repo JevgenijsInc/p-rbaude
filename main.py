acacb58b5f486ea18883421540a1dc7dcd86d000
JevgenijsInc <95209264+JevgenijsInc@users.noreply.github.com>
2022-05-02 06:03:25 +0000

    Ы?

--- main.py
+++ main.py
@@ -2,5 +2,7 @@ brālis = int(input("Ievadiet sava brāļa dzīves gadu skaitu: "))
 māsa = int(input("Ievadiet māsas dzīves gadu skaitu: "))
 if brālis == māsa:
   print("viņi ir dvīņi")
-else:
-  print("tie ir amogus")
+elif brālis > māsa:
+  print("lielais brālis")
+elif brālis < māsa:
+  print("Māsa ir vecāka par brāli")
